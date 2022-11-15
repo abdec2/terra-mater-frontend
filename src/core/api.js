6ea5d39354acf0af966654b01918e516d0fd52c8
abdec2 <abdec2@hotmail.com>
2022-11-15 22:13:09 +0300

    deployed on s3

--- src/core/api.js
+++ src/core/api.js
@@ -1,6 +1,6 @@
 const api = {
     // baseUrl: 'http://localhost:5000', 
-    baseUrl: 'http://ec2-52-45-224-73.compute-1.amazonaws.com:5000',
+    baseUrl: 'https://api.terramaternfts.com',
     nfts: '/api/nfts',
     "nft-v1s": '/api/nft-v1s', 
     nftShowcases: '/api/nft_showcases',
