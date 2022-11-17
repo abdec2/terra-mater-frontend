cf62c7c849a6f744424e2ba7dd4bfe431cfcd996
abdec2 <abdec2@hotmail.com>
2022-11-17 16:21:15 +0300

    Update footer.js

--- src/components/components/footer.js
+++ src/components/components/footer.js
@@ -8,7 +8,7 @@ const footer= () => (
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <div className="de-flex">
+                            <div className="d-flex  align-items-center justify-content-between flex-column flex-lg-row gap-3">
                                 <div className="de-flex-col">
                                     <span onClick={()=> window.open("", "_self")} className="d-flex align-items-center justify-content-center">
                                         <img alt="" className="" style={{width: '50px'}} src="./img/logo.png" />
@@ -25,6 +25,7 @@ const footer= () => (
                                         <span onClick={()=> window.open("", "_self")}><i className="fa fa-rss fa-lg"></i></span>
                                     </div>
                                 </div>
+                                <div className='de-flex-col'>Powered by: <a className='ms-2' href='http://brdigitech.com' target='_blank'>Brdigitech</a></div>
                             </div>
                         </div>
                     </div>
