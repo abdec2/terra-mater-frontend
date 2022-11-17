3294769bc56a1b2b5c2cfa2bce82acf6a9cf1aa0
abdec2 <abdec2@hotmail.com>
2022-11-17 16:43:19 +0300

    scroll to top implemented

--- src/index.js
+++ src/index.js
@@ -17,11 +17,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './store';
 
+import ScrollToTop from './ScrollToTop'
+
 
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
 	</Provider>,
