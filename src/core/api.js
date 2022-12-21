4253c7f0ac6584b423c2409d01d6f4f81ac65f91
abdec2 <abdec2@hotmail.com>
2022-12-21 22:15:27 +0300

    Update api.js

--- src/core/api.js
+++ src/core/api.js
@@ -1,6 +1,6 @@
 const api = {
-    // baseUrl: 'http://localhost:5000', 
-    baseUrl: 'https://api.terramaternfts.com',
+    baseUrl: 'http://aholding.com.bh:5000', 
+    // baseUrl: 'https://api.terramaternfts.com',
     nfts: '/api/nfts',
     "nft-v1s": '/api/nft-v1s', 
     nftShowcases: '/api/nft_showcases',
