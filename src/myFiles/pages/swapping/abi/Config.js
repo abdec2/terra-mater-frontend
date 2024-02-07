52b275cdc84ac2eb29ac407b4a7f32e542b6a8be
Azim Baig <abdec2@hotmail.com>
2024-02-07 22:04:08 +0300

    Update Config.js

--- src/myFiles/pages/swapping/abi/Config.js
+++ src/myFiles/pages/swapping/abi/Config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
   // SwapContractAddress: "0x56940Bc1fa0330797257E50F78f82BaEF38a5033",
   // SwapContractAddress: "0xe51bd36Bc42a3BbDbcf94F931Ecd337eB0FC4582",
   // SwapContractAddress: "0xbA91d12872820a17B2a0bb6F56D4E21C100b7dcd",
-  SwapContractAddress: "0x24fC4A50aBFF949Ad837283Bb78cD64Db761530b",
+  SwapContractAddress: "0x16424Af0C67915708f7bde86B04A45e920a2BBeE",
   NaturaDecimal: "18",
   USDTDecimal: "6",
   Chain_Id: "137",
